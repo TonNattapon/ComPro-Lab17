3cd4cc66e6d8b15c17a44547b67df1481ce1d962
= <=>
2023-02-11 16:22:43 +0700

    Complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,38 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *x,int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(x+i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *x, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(x+i) = rand()%101/100.0;
+    }
+}
+
+void findRowSum(const double *x,double *y,int N,int M){
+	double sum1 = 0.00;
+	for(int i=0; i<N ; i++){
+		for(int j=0; j<M ; j++){
+			sum1 = sum1 + *(x+j+i*M);
+		}
+		*(y+i)=sum1; sum1=0;
+	}
+}
+
+void findColSum(const double *x,double *y,int N,int M){
+    double sum2=0.00;
+    for(int i=0; i<M ;i++){
+        for(int j=0; j<N ;j++){
+            sum2=sum2 + *(x+i+j*M);
+        }
+        *(y+i)=sum2; sum2=0;
+    }
+}
